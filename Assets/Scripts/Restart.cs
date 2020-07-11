a0ceb3a2993c2c2faf47ad616d93ada22ffde463
jooyeokkim <67864645+jooyeokkim@users.noreply.github.com>
2020-07-11 19:37:58 +0900

    Create Restart.cs

---
+++ Assets/Scripts/Restart.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Restart : MonoBehaviour {
+	public Button re;
+	public GameObject loading;
+	public void retry(){
+		loading.SetActive (true);
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex);
+	}
+}
