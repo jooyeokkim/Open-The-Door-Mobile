28063aa1b10e5eeab37eee939f1d56fa7c05e85a
jooyeokkim <67864645+jooyeokkim@users.noreply.github.com>
2020-07-11 15:49:45 +0900

    Update timer.cs

--- Assets/Scripts/timer.cs
+++ Assets/Scripts/timer.cs
@@ -21,14 +21,17 @@ public class timer : MonoBehaviour {
 			time = Time.time - starttime;
 			int intmin = (int)time / 60;
 			int intsecond = (int)time % 60;
-			float millisecond = (time % 1) * 98.3f;
+			float floatmillisecond = (time % 1) * 98.3f;
 			string min = intmin.ToString ();
 			string second = intsecond.ToString ();
+			string millisecond = floatmillisecond.ToString ("f0");
 			if (intmin < 10)
 				min = "0" + min;
 			if (intsecond < 10)
 				second = "0" + second;
-			timertext.text = min + ":" + second + ":" + millisecond.ToString ("f0");
+			if (floatmillisecond < 10)
+				millisecond = "0" + millisecond;
+			timertext.text = min + ":" + second + ":" + millisecond;
 		} else
 			timertext.color = new Color (0.9f, 0.1f, 0.1f);
 	}
