481e8b2eca91238361075dd699564631751867de
jooyeokkim <67864645+jooyeokkim@users.noreply.github.com>
2020-07-11 15:18:38 +0900

    Update Goals.cs

--- Assets/Scripts/Goals.cs
+++ Assets/Scripts/Goals.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Goals : MonoBehaviour {
 	int count;
+	public timer time;
 	AudioSource clear;
 	public GameObject clearpanel;
 	bool isgameover = false;
@@ -17,6 +19,7 @@ public class Goals : MonoBehaviour {
 		if (!isgameover && count == 3) {
 			Debug.Log ("GAME CLEAR!");
 			clear.Play ();
+			time.gameover ();
 			isgameover = true;
 			clearpanel.SetActive (true);
 		}
