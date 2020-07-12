549dc0acc6b83352784ca138d51a32a9fcd26cb9
jooyeokkim <67864645+jooyeokkim@users.noreply.github.com>
2020-07-12 14:57:50 +0900

    Update timer.cs

--- Assets/Scripts/timer.cs
+++ Assets/Scripts/timer.cs
@@ -10,6 +10,7 @@ public class timer : MonoBehaviour {
 	private bool isplaying;
 	private int intmin;
 	private int intsecond;
+	private float floatmillisecond;
 	// Use this for initialization
 	void Start () {
 		timertext = GetComponent<Text> ();
@@ -23,7 +24,7 @@ public class timer : MonoBehaviour {
 			time = Time.time - starttime;
 			intmin = (int)time / 60;
 			intsecond = (int)time % 60;
-			float floatmillisecond = (time % 1) * 98.3f;
+			floatmillisecond = (time % 1) * 100;
 			string min = intmin.ToString ();
 			string second = intsecond.ToString ();
 			string millisecond = floatmillisecond.ToString ("f0");
@@ -33,6 +34,8 @@ public class timer : MonoBehaviour {
 				second = "0" + second;
 			if (floatmillisecond < 10)
 				millisecond = "0" + millisecond;
+			if (floatmillisecond >= 99)
+				millisecond = "99";
 			timertext.text = min + ":" + second + ":" + millisecond;
 		} else
 			timertext.color = new Color (0.9f, 0.1f, 0.1f);
@@ -40,7 +43,7 @@ public class timer : MonoBehaviour {
 	public void gameover(){
 		isplaying = false;
 	}
-	public int getrecord(){
-		return intmin * 60 + intsecond;
+	public float getrecord(){
+		return intmin * 60 + intsecond + floatmillisecond / 100;
 	}	
 }
