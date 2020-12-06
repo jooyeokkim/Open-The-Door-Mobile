0a7c476790a89fd9bbcd7aeda8b97fc875a2a299
jooyeokkim <67864645+jooyeokkim@users.noreply.github.com>
2020-12-06 17:42:22 +0900

    20201206

---
+++ Assets/Scripts/SwitchSiso.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchSiso : MonoBehaviour {
+	public Rigidbody[] blocks;
+	AudioSource push;
+	AudioSource exit;
+	void Start () {
+		var audios = GetComponents<AudioSource> ();
+		push = audios [0];
+		exit = audios [1];
+		for (int i = 0; i < blocks.Length; i++) {
+			if (i % 2 == 0)
+				blocks [i].mass = 10.0f;
+			else
+				blocks [i].mass = 0f;
+		}
+	}
+	void OnTriggerEnter(Collider other){
+		if (!push.isPlaying)
+			push.Play ();
+		for (int i = 0; i < blocks.Length; i++) {
+			if (i % 2 == 1)
+				blocks [i].mass = 10.0f;
+			else
+				blocks [i].mass = 0f;
+		}
+	}
+	void OnTriggerExit(Collider other){
+		if (!exit.isPlaying)
+			exit.Play ();
+		for (int i = 0; i < blocks.Length; i++) {
+			if (i % 2 == 0)
+				blocks [i].mass = 10.0f;
+			else
+				blocks [i].mass = 0f;
+		}
+	}
+}
+
