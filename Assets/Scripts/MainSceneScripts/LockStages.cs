1c76f403a3b0b43b9a0e6a64b29e9cc636a84046
jooyeokkim <67864645+jooyeokkim@users.noreply.github.com>
2021-02-13 14:38:26 +0900

    20210213

---
+++ Assets/Scripts/MainSceneScripts/LockStages.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LockStages : MonoBehaviour {
+	private GameObject level;
+	private Image image;
+	private Button frameButton;
+	private Button imageButton;
+	private GameObject button;
+	private GameObject dia;
+	public Sprite lockimage;
+	// Use this for initialization
+	private int user_level = 2;
+	void Start () {
+		for (int i = user_level + 1; i <= 40; i++) {
+			string levelnum = "LEVEL" + i;
+			level = this.gameObject.transform.Find (levelnum).gameObject;
+			frameButton = level.GetComponent<Button> ();
+			frameButton.interactable = false;
+
+			button = level.transform.Find ("Button").gameObject;
+			dia = level.transform.Find ("userdia").gameObject;
+
+			imageButton = button.GetComponent<Button> ();
+			imageButton.interactable = false;
+
+			image = button.GetComponent<Image> ();
+			image.sprite = lockimage;
+
+			dia.SetActive (false);
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	GameObject GetChildWithName(GameObject obj, string name) {
+		Transform trans = obj.transform;
+		Transform childTrans = trans.Find (name);
+		if (childTrans != null) {
+			return childTrans.gameObject;
+		} else {
+			return null;
+		}
+	}
+}
